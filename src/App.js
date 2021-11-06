b9fa1974e114df47a146293c3e3dcaf94286b8c7
TranTrungTinLV <trungtintin1989@gmail.com>
2021-11-06 17:11:36 +0700

    thuc_an Menu

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import Thucan from './tailwindcss/Thucan';
+
+
+function App() {
+  return (
+    <div className="App">
+        <Thucan/>
+    </div>
+  );
+}
+
+export default App;
